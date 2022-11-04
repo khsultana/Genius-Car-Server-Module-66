c7013b7c9837a69b4264d48e8e5007110b21ced9
khsultana <khsultana1995@gmail.com>
2022-11-04 23:04:50 +0600

    basic nodemongo saver

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
-const express = require('express')
-const cors = require('cors')
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+//user:dbUser2
+//password:8u2s29iSvbUyz1g1
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ybtjl3s.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
-app.get('/', (req, res) => {
-    res.send('The Port is running')
-})
+app.get("/", (req, res) => {
+  res.send("The Port is running");
+});
 
 app.listen(port, () => {
-    console.log(`API Post Running , ${port}`)
-})
+  console.log(`API Post Running , ${port}`);
+});
